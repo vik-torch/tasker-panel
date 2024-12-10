bd5ecd186960581f92fcc004232041ee3c463e65
vik-torch <chaikinviktor2018@mail.ru>
2024-12-10 17:54:16 +0300

    clear up rubbish

--- index.php
+++ index.php
@@ -1,11 +1,5 @@
 <?php
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-use Core\Route\Router;
-
 require_once __DIR__ . '/vendor/autoload.php';
 
 try {
