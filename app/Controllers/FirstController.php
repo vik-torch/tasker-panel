f8cecdfa2bd93b3e778945b67385124a08d98c90
vik-torch <chaikinviktor2018@mail.ru>
2024-12-12 02:47:26 +0300

    testing view

--- app/Controllers/FirstController.php
+++ app/Controllers/FirstController.php
@@ -3,4 +3,12 @@
 namespace App\Controllers;
 
 class FirstController extends Controller
-{}
+{
+    public function index(): string
+    {
+        $name = 'Viktor';
+        $comp = compact('name');
+        $response = $this->view::render('first', compact('name'));
+        return $response;
+    }
+}
