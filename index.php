64aea5f4a920e7daaebaab57612864ae68e061b2
vik-torch <chaikinviktor2018@mail.ru>
2024-12-12 11:37:22 +0300

    load ENV

--- index.php
+++ index.php
@@ -3,6 +3,11 @@
 require_once __DIR__ . '/vendor/autoload.php';
 
 try {
+    // Загрузка переменных из файла .env
+    $dotenv = \Dotenv\Dotenv::createImmutable(__DIR__);
+    $dotenv->load();
+
+    // Перенаправление запроса на роуты
     require_once __DIR__ . '/app/routes.php';
 
     $url = $_SERVER['REQUEST_URI'];
